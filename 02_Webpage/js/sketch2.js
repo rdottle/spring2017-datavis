597d900073f920e00bfc58e853882db918fbb343
rdottle <rachaelcdottle@gmail.com>
2017-02-27 09:56:00 -0500

    edits from class 2/24

---
+++ 02_Webpage/js/sketch2.js
@@ -0,0 +1,21 @@
+
+//created variables
+var MyData = [12, 3, 56, 607, 23]
+var labels = ['Colombia', 'Brazil', 'Peru', 'Chile', 'Venezuela']
+
+
+//create canvas
+
+function setup(){
+	createCanvas(800, 800);
+	console.log('Setup complete...');
+
+}
+
+
+// Draw function 
+
+function draw(){
+
+	
+}
